646c3e1ba1058941d8dae3af1630945546c865b9
Shayan Mostafaei <137507271+shayanmostafaei@users.noreply.github.com>
2025-10-21 14:34:09 +0200

    Update 04_SHAP_Analysis.py

--- scripts/04_SHAP_Analysis.py
+++ scripts/04_SHAP_Analysis.py
@@ -73,7 +73,7 @@ X = X.rename(columns=rename_dict)
 # 4. Train/test split (stratified)
 # ----------------------------------------------------------------------------------
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
+    X, y, test_size=0.3, random_state=42, stratify=y
 )
 
 # ----------------------------------------------------------------------------------
